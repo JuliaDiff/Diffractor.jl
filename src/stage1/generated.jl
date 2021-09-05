9624791af51ed1f718fc5a456dbb82b8bd43ab8d
GiggleLiu <cacate0129@gmail.com>
2021-09-05 05:47:55 -0400

    fix array indexing backward rule

--- src/stage1/generated.jl
+++ src/stage1/generated.jl
@@ -313,6 +313,7 @@ function (::∂⃖{N})(::typeof(Base.getindex), a::Array, inds...) where {N}
     getindex(a, inds...), let
         EvenOddOdd{1, c_order(N)}(
             (@Base.aggressive_constprop Δ->begin
+                Δ isa ZeroTangent && return (NoTangent(), NoTangent(), map(x->NoTangent(), inds)...)
                 BB = zero(a)
                 BB[inds...] = Δ
                 (NoTangent(), BB, map(x->NoTangent(), inds)...)

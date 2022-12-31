6495ed1f2a38ffd793d8d356530ff2a0c5fa5796
Keno Fischer <keno@juliacomputing.com>
2022-12-31 20:25:20 +0000

    Hookup demand-driven forward mode to the Diffractor runtime
    Tests currently depend on https://github.com/JuliaLang/julia/pull/48045
    and https://github.com/JuliaLang/julia/pull/48059, so we should either
    get those merged first, or mark them here as broken.

--- src/stage2/forward.jl
+++ src/stage2/forward.jl
@@ -2,7 +2,7 @@ using .CC: compact!
 
 # Engineering entry point for the 2nd-order forward AD functionality. This is
 # unlikely to be the actual interface. For now, it is used for testing.
-function dontuse_nth_order_forward_stage2(tt::Type)
+function dontuse_nth_order_forward_stage2(tt::Type, order::Int=1)
     interp = ADInterpreter(; forward=true, backward=false)
     match = Base._which(tt)
     frame = Core.Compiler.typeinf_frame(interp, match.method, match.spec_types, match.sparams, #=run_optimizer=#true)
@@ -10,26 +10,44 @@ function dontuse_nth_order_forward_stage2(tt::Type)
     ir = copy((interp.opt[0][frame.linfo].inferred).ir::IRCode)
 
     # Find all Return Nodes
-    vals = SSAValue[]
+    vals = Pair{SSAValue, Int}[]
     for i = 1:length(ir.stmts)
         if isa(ir[SSAValue(i)][:inst], ReturnNode)
-            push!(vals, SSAValue(i))
+            push!(vals, SSAValue(i)=>order)
         end
     end
 
-    function custom_diff!(ir, ssa, stmt, recurse)
+    function visit_custom!(ir::IRCode, @nospecialize(stmt), order, recurse)
         if isa(stmt, ReturnNode)
-            r = recurse(stmt.val)
-            ir[ssa][:inst] = ReturnNode(r)
-            return ssa
+            recurse(stmt.val)
+            return true
         elseif isa(stmt, Argument)
-            return 1.0
+            return true
+        else
+            return false
         end
-        return nothing
     end
+
+    function transform!(ir::IRCode, ssa::SSAValue, _)
+        inst = ir[ssa]
+        stmt = inst[:inst]
+        if isa(stmt, ReturnNode)
+            nr = insert_node!(ir, ssa, NewInstruction(Expr(:call, getindex, stmt.val, TaylorTangentIndex(order)), Any))
+            inst[:inst] = ReturnNode(nr)
+        else
+            error()
+        end
+    end
+
+    function transform!(ir::IRCode, arg::Argument, _)
+        return insert_node!(ir, SSAValue(1), NewInstruction(Expr(:call, ∂xⁿ{order}(), arg), typeof(∂xⁿ{order}()(1.0))))
+    end
+
 
     irsv = CC.IRInterpretationState(interp, ir, frame.linfo, CC.get_world_counter(interp), ir.argtypes[1:frame.linfo.def.nargs])
-    forward_diff!(ir, interp, irsv, vals; custom_diff!)
+    ir = forward_diff!(ir, interp, frame.linfo, CC.get_world_counter(interp), vals; visit_custom!, transform!)
+
+    display(ir)
 
     ir = compact!(ir)
     return OpaqueClosure(ir)

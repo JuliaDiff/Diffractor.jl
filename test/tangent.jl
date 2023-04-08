018ceb59fb58837b0cb0aa4d8977205c4f8948c8
Keno Fischer <keno@juliacomputing.com>
2023-04-08 08:27:29 +0000

    Fix test

--- test/tangent.jl
+++ test/tangent.jl
@@ -18,7 +18,7 @@ using Test
         @test repr(ZeroBundle{1}) == "ZeroBundle{1}"
         @test repr(ZeroBundle{1, Float64}) == "ZeroBundle{1, Float64}"
 
-        @test repr(typeof(DNEBundle{1}(getfield))) == DNEBundle{1, typeof(getfield)}
+        @test repr(typeof(DNEBundle{1}(getfield))) == "DNEBundle{1, typeof(getfield)}"
     end
 end
 

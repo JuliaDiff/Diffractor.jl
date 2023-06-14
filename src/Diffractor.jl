53daa5758a375ad7f74b99cd4016120cd762ea67
Cody Tapscott <topolarity@tapscott.me>
2023-06-14 09:58:38 -0400

    Use `@recompile_invalidations` to heal Base/Core.Compiler invalidations
    This increases the pre-compile time to ~29s on my machine.
    
    Fixes significant compilation lag for simple cases like:
    ```
    julia> using Diffractor
    julia> Int64
    Int64
    ```

--- src/Diffractor.jl
+++ src/Diffractor.jl
@@ -1,6 +1,7 @@
 module Diffractor
 
 using StructArrays
+using PrecompileTools
 
 export ∂⃖, gradient
 
@@ -8,35 +9,37 @@ const CC = Core.Compiler
 
 const GENERATORS = Expr[]
 
-include("runtime.jl")
-include("interface.jl")
-include("utils.jl")
-include("tangent.jl")
-include("jet.jl")
+@recompile_invalidations begin
+    include("runtime.jl")
+    include("interface.jl")
+    include("utils.jl")
+    include("tangent.jl")
+    include("jet.jl")
 
-include("stage1/generated.jl")
-include("stage1/forward.jl")
-include("stage1/recurse_fwd.jl")
-include("stage1/mixed.jl")
-include("stage1/broadcast.jl")
+    include("stage1/generated.jl")
+    include("stage1/forward.jl")
+    include("stage1/recurse_fwd.jl")
+    include("stage1/mixed.jl")
+    include("stage1/broadcast.jl")
 
-include("stage2/interpreter.jl")
-include("stage2/lattice.jl")
-include("stage2/abstractinterpret.jl")
-include("stage2/tfuncs.jl")
-include("stage2/forward.jl")
+    include("stage2/interpreter.jl")
+    include("stage2/lattice.jl")
+    include("stage2/abstractinterpret.jl")
+    include("stage2/tfuncs.jl")
+    include("stage2/forward.jl")
 
-include("codegen/forward.jl")
-include("analysis/forward.jl")
-include("codegen/forward_demand.jl")
-include("codegen/reverse.jl")
+    include("codegen/forward.jl")
+    include("analysis/forward.jl")
+    include("codegen/forward_demand.jl")
+    include("codegen/reverse.jl")
 
-include("extra_rules.jl")
+    include("extra_rules.jl")
 
-include("higher_fwd_rules.jl")
+    include("higher_fwd_rules.jl")
 
-include("debugutils.jl")
+    include("debugutils.jl")
 
-include("stage1/termination.jl")
+    include("stage1/termination.jl")
+end
 
 end

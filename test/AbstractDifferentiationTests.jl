b60acd4aa027f6598020f81bc51ad238105b27db
Frames White <me@oxinabox.net>
2023-08-21 13:20:23 +0800

    add more types supported with Bundle helper

--- test/AbstractDifferentiationTests.jl
+++ test/AbstractDifferentiationTests.jl
@@ -1,6 +1,24 @@
-using AbstractDifferentiation, Diffractor, Test, LinearAlgebra
+using AbstractDifferentiation, Diffractor, Test, LinearAlgebra, ChainRulesCore
 import AbstractDifferentiation as AD
 backend = Diffractor.DiffractorForwardBackend()
+
+@testset "bundle" begin
+    bundle = Diffractor.bundle
+
+    @test bundle(1.0, 2.0) isa Diffractor.TaylorBundle{1}
+    @test bundle([1.0, 2.0], [2.0, 3.0]) isa Diffractor.TaylorBundle{1}
+    @test bundle(1.5=>2.5, Tangent{Pair{Float64, Float64}}(first=1.0, second=2.0)) isa Diffractor.CompositeBundle{1}
+    @test bundle(1.1, ChainRulesCore.ZeroTangent()) isa Diffractor.ZeroBundle{1}
+    @test bundle(1.5=>2.5=>3.5, Tangent{Pair{Float64, Pair{Float64, Float64}}}(first=1.0, second=Tangent{Pair{Float64, Float64}}(first=1.0, second=2.0))) isa Diffractor.CompositeBundle{1}
+    
+    # noncanonical structural tangent
+    b = bundle(1.5=>2.5=>3.5, Tangent{Pair{Float64, Pair{Float64, Float64}}}(second=Tangent{Pair{Float64, Float64}}(second=2.0, first=1.0)))
+    t = Diffractor.first_partial(b)
+    @test b isa Diffractor.CompositeBundle{1}
+    @test iszero(t.first)
+    @test t.second.first == 1.0
+    @test t.second.second == 2.0
+end
 
 @testset "basics" begin
     @test AD.derivative(backend, +, 1.5, 10.0) == (1.0, 1.0)
@@ -50,3 +68,4 @@ include(joinpath(pathof(AbstractDifferentiation), "..", "..", "test", "test_util
         end
     end
 end
+

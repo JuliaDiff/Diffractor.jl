199cac4813c02150c839096537237bfd25abcfef
Frames White <me@oxinabox.net>
2023-10-05 23:22:49 +0800

    add extra second order test

--- test/forward_mutation.jl
+++ test/forward_mutation.jl
@@ -22,7 +22,7 @@ end
     @test 🥯[TaylorTangentIndex(2)].x == 10.0
 end
 
-@testset "basis struct work" begin
+@testset "basis struct work: double" begin
     function double!(val::MDemo1)
         val.x *= 2.0
         return val
@@ -44,6 +44,27 @@ end
     @test 🐇[TaylorTangentIndex(2)].x == 0.0  # returns 20
 end
 
+@testset "basis struct work: square" begin
+    function square!(val::MDemo1)
+        val.x ^= 2.0
+        return val
+    end
+    function wrap_and_square(x)
+        val = MDemo1(x)
+        square!(val)
+    end
+    # first derivative
+    🐰 = ∂☆{1}()(ZeroBundle{1}(wrap_and_square), TaylorBundle{1}(10.0, (1.0,)))
+    @test first_partial(🐰) isa MutableTangent{MDemo1}
+    @test first_partial(🐰).x == 20.0
+
+    # second derivative
+    🐇 = ∂☆{2}()(ZeroBundle{2}(wrap_and_square), TaylorBundle{2}(1, (1.0, 0.0)))
+    @test 🐇[TaylorTangentIndex(1)] isa MutableTangent{MDemo1}
+    @test 🐇[TaylorTangentIndex(1)].x == 20.0
+    @test 🐇[TaylorTangentIndex(2)] isa MutableTangent
+    @test 🐇[TaylorTangentIndex(2)].x == 2.0  # returns 20
+end
 
 
 # end # module
